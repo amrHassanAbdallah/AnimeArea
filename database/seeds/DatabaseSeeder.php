1bfc55052a8d5cdea90cbecb6f37c56437d8b3fb
Amr H <amrh434@gmail.com>
2017-11-26 16:26:04 +0200

    readme updated , Database seeder edit to create some dummy data.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         /*$this->call(UsersTableSeeder::class);
         $this->call(ProductsTableSeeder::class);*/
 /*        $this->call(UsersTableSeeder::class);*/
-/*        $this->call(UsersTableSeeder::class);*/
+        $this->call(UsersTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
     }
 }
