aa93e8e730f27e47ef4b5c299c0e41e4aa69fdd4
Amr H <amrh434@gmail.com>
2017-12-18 11:34:32 +0200

    state function implementation

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\classes\Checkout;
+use App\Order;
 use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
-    //
+    public function state($order_id)
+    {
+        //send notification to user , chaange order state , change delevery state
+        $Order = Order::find($order_id);
+        $Order->state = 0;
+        $Order->shipment_state = "In progress";
+        $Order->save();
+        $login = new Checkout();
+        $login->attach([new \App\classes\OrderConfirmationNotifier($Order->id)]);
+        $login->fire();
+        return redirect()->back()->with("success","Order state have been changed ");
+
+    }
 }
