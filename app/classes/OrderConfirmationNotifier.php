1340443db361f7b58ff0dacd455576e2910d4594
Amr H <amrh434@gmail.com>
2017-12-18 11:35:09 +0200

    notifier to send notification to the customer when their order state change

--- app/classes/OrderConfirmationNotifier.php
+++ app/classes/OrderConfirmationNotifier.php
@@ -9,7 +9,29 @@
 namespace App\classes;
 
 
-class OrderConfirmationNotifier
-{
+use App\Notification;
+use App\Order;
 
+class OrderConfirmationNotifier implements Observer
+{
+    protected  $Order_id;
+    public function __construct($order_id)
+    {
+      $this->Order_id = $order_id;
+    }
+
+    public function handle()
+    {
+        $order = Order::find($this->Order_id);
+        $user = $order->getUser();
+
+        $Notification = new Notification;
+        $Notification->content = $order->id;
+        /*"a user :".Auth::user()->name." , email:  ".Auth::user()->email." has placed an order  ,".$order->description;*/
+        $Notification->user_id = $user->id;
+        $Notification->type = "Shipment_state";
+        $Notification->content =  "Your order #{$order->id} state has bee changed to {$order->shipment_state}";
+        $Notification->save();
+
+    }
 }
