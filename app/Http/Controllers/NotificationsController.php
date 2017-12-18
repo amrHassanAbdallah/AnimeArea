75ff3942656ee42f4b80dd084154fa52b5fb3857
Amr H <amrh434@gmail.com>
2017-12-18 11:03:48 +0200

    the view for the order if the notification type is order .

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Notification;
+use App\Order;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,11 +13,15 @@ class NotificationsController extends Controller
     public function single($id)
     {
         $notification = Notification::find($id);
+
         if(!$notification->saw){
             $notification->saw = 1;
             $notification->save();
         }
-
+       if($notification->type == "order"){
+            $order = Order::find((int)$notification->content);
+           return view('notifications.order')->with(['order'=>$order,'customer'=>$order->getUser()]);
+       }
         return view('notifications.single')->with('notification',Notification::find($id));
     }
 
