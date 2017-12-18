b0278a739a9fadcf0423901c87eb1a4588b55a4e
Amr H <amrh434@gmail.com>
2017-12-18 13:13:16 +0200

    tax class , decrator

--- app/classes/Tax.php
+++ app/classes/Tax.php
@@ -9,7 +9,28 @@
 namespace App\classes;
 
 
-class Tax
+class Tax implements ProductService
 {
+    protected $Order;
+    protected $percent ;
 
+    public function __construct(ProductService $car,$number = 10)
+    {
+        $this->Order = $car;
+        $this->percent = $number;
+    }
+
+    public function getCost()
+    {
+        return $this->Order->getCost() + ($this->Order->getCost()*$this->percent)/100;
+    }
+    public function getDescription()
+    {
+        return $this->Order->getDescription()." ,  ".$this->percent."% taxes : Tax";
+    }
+
+    public function setQNTY($number)
+    {
+        $this->percent = $number;
+    }
 }
