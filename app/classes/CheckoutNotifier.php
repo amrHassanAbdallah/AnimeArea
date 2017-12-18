5394723555074245ff38282cad891b2476e4eb01
Amr H <amrh434@gmail.com>
2017-12-18 13:44:44 +0200

    update the saw value if the seller see the order and didn't confirm it , if saw it and confirm it wouldn't send the notification again .

--- app/classes/CheckoutNotifier.php
+++ app/classes/CheckoutNotifier.php
@@ -17,12 +17,22 @@ class CheckoutNotifier implements Observer
 {
     public function handle()
     {
+
         $order = Order::where("state","=",1)->where("cart_id","=",Auth::user()->cart->id)->first();
-        $Notification = new Notification;
-        $Notification->content = $order->id;
+        $Notification = Notification::where("type","=","order")->where("user_id","=",1)->where("content","=",$order->id)->first();
+        if($Notification  === null){
+            $Notification = new Notification;
+            $Notification->content = $order->id;
             /*"a user :".Auth::user()->name." , email:  ".Auth::user()->email." has placed an order  ,".$order->description;*/
-        $Notification->user_id = 1;
-        $Notification->save();
+            $Notification->user_id = 1;
+            $Notification->save();
+        }else{
+            if($Notification->saw){
+                $Notification->saw = 0;
+                $Notification->save();
+            }
+        }
+
 
     }
 }
