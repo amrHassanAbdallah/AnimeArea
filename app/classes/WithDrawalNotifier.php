dae283a51bafa2e8e26ed09b00a2409341a1b1a7
Amr H <amrh434@gmail.com>
2017-12-19 22:47:28 +0200

    orderdeleted and Withdrawal Notifiers starting files .

---
+++ app/classes/WithDrawalNotifier.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amr
+ * Date: 12/18/17
+ * Time: 10:18 AM
+ */
+
+namespace App\classes;
+
+
+use App\Notification;
+use App\Order;
+
+class WithDrawalNotifier implements Observer
+{
+    protected  $Order_id;
+    public function __construct($order_id)
+    {
+      $this->Order_id = $order_id;
+    }
+
+    public function handle()
+    {
+        $order = Order::find($this->Order_id);
+        $user = $order->getUser();
+
+        $Notification = new Notification;
+        $Notification->content = $order->id;
+        /*"a user :".Auth::user()->name." , email:  ".Auth::user()->email." has placed an order  ,".$order->description;*/
+        $Notification->user_id = $user->id;
+        $Notification->type = "Shipment_state";
+        $Notification->content =  "Your order #  <a href=\"/Orders/{$order->id}\"> {$order->id} </a> state has been changed to {$order->shipment_state}";
+
+        $Notification->save();
+
+    }
+}
